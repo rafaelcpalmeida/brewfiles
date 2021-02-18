d4be3e23dc47209174e76869fc97483c4dbf5ada
Rafael Almeida <rafaelcpalmeida@pm.me>
2021-02-18 16:17:06 +0000

    Add initial files

---
+++ Brewfile
@@ -0,0 +1,21 @@
+cask_args appdir: "/Applications"
+
+### TAPS
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/cask-fonts"
+tap "homebrew/core"
+
+### FORMULAS
+
+# Tools
+brew "tree" # Display directories as trees (with optional color/HTML output)
+brew "wget" # Internet file retriever
+
+# Git
+brew "git" # Distributed revision control system
+brew "git-extras" # Small git utilities
+brew "git-lfs" # Git extension for versioning large files
+
+# Languages
+brew "python", link: true
---
+++ brew-install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Installs Homebrew and some of the common dependencies needed/desired for software development
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Check for Homebrew and install it if missing
+if test ! $(which brew)
+then
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew tap homebrew/bundle
+brew tap homebrew/cask
+brew tap homebrew/cask-fonts
+brew tap homebrew/core
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# Install the Homebrew packages I use on a day-to-day basis.
+brew bundle install --file=Brewfile --verbose
+
+# INSTAL CASK APPLICATIONS
+
+brew bundle install --file=Brewfile-cask --verbose
+
+# Remove outdated versions from the cellar
+brew cleanup
